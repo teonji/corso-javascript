f2cd8d9e215d0ff4605855af3666730299fc89e6
matteo.tritto <matteo.tritto@weroad.it>
2023-04-08 23:21:08 +0200

    fix back button

--- pages/corso/[chapter]/[step].vue
+++ pages/corso/[chapter]/[step].vue
@@ -177,7 +177,6 @@ useHead({
             <nuxt-link
               :to="prevStep ? `/corso${prevStep.path}` : `/corso${current.path}`"
               class="inline-flex items-center justify-center p-2.5 mx-2 font-medium rounded-full group focus:ring-4 focus:ring-persian-green focus:outline-none focus:ring-saffron"
-              @click="goNextStep()"
             >
               <svg class="w-5 h-5 text-gray-300 group-hover:text-gray-900 group-hover:text-white" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512" aria-hidden="true">
                 <path d="M267.5 440.6c9.5 7.9 22.8 9.7 34.1 4.4s18.4-16.6 18.4-29V96c0-12.4-7.2-23.7-18.4-29s-24.5-3.6-34.1 4.4l-192 160L64 241V96c0-17.7-14.3-32-32-32S0 78.3 0 96V416c0 17.7 14.3 32 32 32s32-14.3 32-32V271l11.5 9.6 192 160z" fill="currentColor" />
